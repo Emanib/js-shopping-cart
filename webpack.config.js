c647bc6d082a7bc3c73b8046d03d2a0215b34fec
Mohammed F Safadi <m@safadi.ps>
2021-10-07 08:23:08 +0300

    update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ module.exports = {
     mode: "development",
     entry: './src/index.js',
     output: {
-        filename: 'todo.js',
+        filename: 'cart.js',
         path: path.resolve(__dirname, 'dist/js'),
     },
 };
